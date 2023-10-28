30da3f435a1a12e51730054395aa8975e6700520
Ruslan Popov <ruslan_popov_2014@inbox.ru>
2023-10-28 22:40:38 +0300

    initial commit

---
+++ src/main/java/person/people/spring/controllers/AdminController.java
@@ -0,0 +1,61 @@
+package person.people.spring.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import person.people.spring.dao.PersonDAO;
+import person.people.spring.models.Person;
+
+import javax.validation.constraints.Min;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public AdminController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String adminPage(Model model, @ModelAttribute("person") Person person) {
+        model.addAttribute("people", personDAO.index());
+
+        return "adminPage";
+
+    }
+
+
+
+    @PatchMapping("/add")
+    public String makeAdmin(@ModelAttribute("person") Person person) {
+        System.out.println(person.getId());
+
+        return "redirect:/people";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
